1779b5effebc69077fceaa11ef43a65635e8ee3a
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-09-13 16:22:50 -0700

    started js

---
+++ script.js
@@ -0,0 +1,8 @@
+`use strict`;
+
+const form = document.querySelector('.form');
+const containerEvents = document.querySelector('.events');
+const inputEvent = document.querySelector(`.form-input--event`);
+const inputDate = document.querySelector(`.form-input--date`);
+const inputStartTime = document.querySelector(`.form-input--startTime`);
+const inputEndTime = document.querySelector(`.form-input--endTime`);
