2c50435e7b6ae1319d742ad850ae9eab2f006f2b
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-09-13 17:38:38 -0700

    tried out api

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ const containerEvents = document.querySelector('.events');
 const inputEvent = document.querySelector(`.form-input--event`);
 const inputDate = document.querySelector(`.form-input--date`);
 const inputStartTime = document.querySelector(`.form-input--startTime`);
-const inputEndTime = document.querySelector(`.form-input--endTime`);
+const inputEndTime = document.querySelector(`.form-input--endTime`); 
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(latitude, longitude); 
+
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+    }, function () {
+        alert('Could not get your position');
+    });
+}   
